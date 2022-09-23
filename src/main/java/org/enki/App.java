94994d336a6c18cd7d0f64eefd3c6650987283f6
Gene McCulley <mcculley@stackframe.com>
2022-09-23 15:15:29 -0400

    Refactor.

--- src/main/java/org/enki/App.java
+++ src/main/java/org/enki/App.java
@@ -18,6 +18,7 @@ import javax.swing.text.Highlighter;
 import javax.swing.text.JTextComponent;
 import javax.swing.text.Utilities;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.List;
@@ -42,6 +43,211 @@ public class App {
     private static boolean isURL(final String s) {
         return s.startsWith("http://") ||
                 s.startsWith("https://"); // FIXME: Detect domain in accordance with Twitter rules.
+    }
+
+    private class DocumentFrame extends JFrame {
+
+        private class ContentPane extends JTextPane {
+
+            public ContentPane() {
+                setToolTipText("");
+            }
+
+            @Override
+            public String getToolTipText(final MouseEvent event) {
+                final int position = viewToModel2D(event.getPoint());
+                return Integer.toString(position);
+            }
+
+        }
+
+        public DocumentFrame() throws HeadlessException {
+            super("Parsimonious Publisher");
+
+            final JTextPane contentArea = new ContentPane();
+            final JLabel wordLabel = new JLabel();
+            final JList<RuleMatch> errorList = new JList<>();
+
+            final AtomicReference<CaretListener> contentCaretListener = new AtomicReference<>();
+
+            final ListSelectionListener errorListListener = e -> {
+                final List<RuleMatch> selectedMatches = errorList.getSelectedValuesList();
+                if (!e.getValueIsAdjusting() && selectedMatches.size() == 1) {
+                    final RuleMatch m = selectedMatches.get(0);
+                    contentArea.removeCaretListener(contentCaretListener.get());
+                    contentArea.setCaretPosition(m.getToPos());
+                    contentArea.addCaretListener(contentCaretListener.get());
+                    contentArea.requestFocus();
+                }
+            };
+
+            errorList.setCellRenderer(new TransformingListCellRenderer<>(
+                    (Function<RuleMatch, String>) ruleMatch -> {
+                        final String region = getRegion(contentArea, ruleMatch);
+                        return region + ": " +
+                                ruleMatch.getMessage().replace("<suggestion>", "'").replaceAll("</suggestion>", "'");
+                    }));
+
+            errorList.addListSelectionListener(errorListListener);
+
+            contentCaretListener.set(e -> {
+                final int selectionStart = contentArea.getSelectionStart();
+                final int selectionEnd = contentArea.getSelectionEnd();
+                final int dot = e.getDot();
+                final int mark = e.getMark();
+
+                if (mark == dot) {
+                    final String wordAtCaret = getWordAtCaret(contentArea, dot);
+                    wordLabel.setText(wordAtCaret);
+                } else {
+                    wordLabel.setText(contentArea.getText()
+                            .substring(contentArea.getSelectionStart(), contentArea.getSelectionEnd()));
+                }
+
+                try {
+                    final Highlighter h = contentArea.getHighlighter();
+                    h.removeAllHighlights();
+                    final List<RuleMatch> r = languageTool.check(getText(contentArea));
+                    errorList.setModel(new ListModel<>() {
+
+                        @Override
+                        public int getSize() {
+                            return r.size();
+                        }
+
+                        @Override
+                        public RuleMatch getElementAt(final int index) {
+                            return r.get(index);
+                        }
+
+                        @Override
+                        public void addListDataListener(ListDataListener l) {
+                        }
+
+                        @Override
+                        public void removeListDataListener(ListDataListener l) {
+                        }
+
+                    });
+
+                    int ruleRow = 0;
+                    for (final RuleMatch m : r) {
+                        final RuleMatch.Type type = m.getType();
+                        final Highlighter.HighlightPainter painter = switch (type) {
+                            case Hint -> new DefaultHighlighter.DefaultHighlightPainter(Color.LIGHT_GRAY);
+                            case UnknownWord -> new DefaultHighlighter.DefaultHighlightPainter(Color.RED);
+                            case Other -> new DefaultHighlighter.DefaultHighlightPainter(Color.YELLOW);
+                        };
+
+                        try {
+                            h.addHighlight(m.getFromPos(), m.getToPos(), painter);
+                        } catch (final BadLocationException ex) {
+                            throw new AssertionError(ex);
+                        }
+
+                        if (dot >= m.getFromPos() && dot <= m.getToPos()) {
+                            errorList.removeListSelectionListener(errorListListener);
+                            errorList.setSelectedIndex(ruleRow);
+                            errorList.addListSelectionListener(errorListListener);
+                        }
+
+                        ruleRow++;
+                    }
+                } catch (final IOException ex) {
+                    throw new UncheckedIOException(ex);
+                }
+
+                if (dot != mark) {
+                    contentArea.removeCaretListener(contentCaretListener.get());
+                    contentArea.setSelectionStart(selectionStart);
+                    contentArea.setSelectionEnd(selectionEnd);
+                    contentArea.addCaretListener(contentCaretListener.get());
+                }
+            });
+
+            contentArea.addCaretListener(contentCaretListener.get());
+
+            final JComponent metaContainer = new Box(BoxLayout.Y_AXIS);
+
+            final JTable infoTable = new JTable();
+
+            infoTable.setModel(new DefaultTableModel() {
+
+                @Override
+                public int getRowCount() {
+                    return 3;
+                }
+
+                @Override
+                public int getColumnCount() {
+                    return 2;
+                }
+
+                @Override
+                public boolean isCellEditable(final int rowIndex, final int columnIndex) {
+                    return false;
+                }
+
+                @Override
+                public Object getValueAt(final int rowIndex, final int columnIndex) {
+                    return switch (rowIndex) {
+                        case 0 -> switch (columnIndex) {
+                            case 0 -> "Characters";
+                            case 1 -> Integer.toString(getText(contentArea).length());
+                            default -> throw new AssertionError();
+                        };
+                        case 1 -> switch (columnIndex) {
+                            case 0 -> "Twitter Characters";
+                            case 1 -> Integer.toString(getTwitterCharacters(getText(contentArea)));
+                            default -> throw new AssertionError();
+                        };
+                        case 2 -> switch (columnIndex) {
+                            case 0 -> "Twitter Characters Remaining";
+                            case 1 -> Integer.toString(280 - getTwitterCharacters(getText(contentArea)));
+                            default -> throw new AssertionError();
+                        };
+                        default -> throw new AssertionError();
+                    };
+                }
+
+            });
+
+            metaContainer.add(infoTable);
+
+            final JPanel caretWord = new JPanel();
+            caretWord.add(wordLabel);
+
+            metaContainer.add(caretWord);
+
+            metaContainer.add(new JScrollPane(errorList));
+
+            final JSplitPane mainSplitPane =
+                    new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, metaContainer, new JScrollPane(contentArea));
+
+            add(mainSplitPane);
+
+            contentArea.getDocument().addDocumentListener(new DocumentListener() {
+
+                @Override
+                public void insertUpdate(final DocumentEvent e) {
+                    ((DefaultTableModel) infoTable.getModel()).fireTableDataChanged();
+                }
+
+                @Override
+                public void removeUpdate(final DocumentEvent e) {
+                    ((DefaultTableModel) infoTable.getModel()).fireTableDataChanged();
+                }
+
+                @Override
+                public void changedUpdate(final DocumentEvent e) {
+                    ((DefaultTableModel) infoTable.getModel()).fireTableDataChanged();
+                }
+
+            });
+
+
+        }
+
     }
 
     private static int getTwitterCharacters(final String s) {
@@ -77,189 +283,7 @@ public class App {
     }
 
     private void start() {
-        final JTextPane contentArea = new JTextPane();
-        final JLabel wordLabel = new JLabel();
-        final JList<RuleMatch> errorList = new JList<>();
-
-        final AtomicReference<CaretListener> contentCaretListener = new AtomicReference<>();
-
-        final ListSelectionListener errorListListener = e -> {
-            final List<RuleMatch> selectedMatches = errorList.getSelectedValuesList();
-            if (!e.getValueIsAdjusting() && selectedMatches.size() == 1) {
-                final RuleMatch m = selectedMatches.get(0);
-                contentArea.removeCaretListener(contentCaretListener.get());
-                contentArea.setCaretPosition(m.getToPos());
-                contentArea.addCaretListener(contentCaretListener.get());
-                contentArea.requestFocus();
-            }
-        };
-
-        errorList.setCellRenderer(new TransformingListCellRenderer<>(
-                (Function<RuleMatch, String>) ruleMatch -> {
-                    final String region = getRegion(contentArea, ruleMatch);
-                    return region + ": " +
-                            ruleMatch.getMessage().replace("<suggestion>", "'").replaceAll("</suggestion>", "'");
-                }));
-
-        errorList.addListSelectionListener(errorListListener);
-
-        contentCaretListener.set(e -> {
-            final int selectionStart = contentArea.getSelectionStart();
-            final int selectionEnd = contentArea.getSelectionEnd();
-            final int dot = e.getDot();
-            final int mark = e.getMark();
-
-            if (mark == dot) {
-                final String wordAtCaret = getWordAtCaret(contentArea, dot);
-                wordLabel.setText(wordAtCaret);
-            } else {
-                wordLabel.setText(contentArea.getText()
-                        .substring(contentArea.getSelectionStart(), contentArea.getSelectionEnd()));
-            }
-
-            try {
-                final Highlighter h = contentArea.getHighlighter();
-                h.removeAllHighlights();
-                final List<RuleMatch> r = languageTool.check(getText(contentArea));
-                errorList.setModel(new ListModel<>() {
-
-                    @Override
-                    public int getSize() {
-                        return r.size();
-                    }
-
-                    @Override
-                    public RuleMatch getElementAt(final int index) {
-                        return r.get(index);
-                    }
-
-                    @Override
-                    public void addListDataListener(ListDataListener l) {
-                    }
-
-                    @Override
-                    public void removeListDataListener(ListDataListener l) {
-                    }
-
-                });
-
-                int ruleRow = 0;
-                for (final RuleMatch m : r) {
-                    final RuleMatch.Type type = m.getType();
-                    final Highlighter.HighlightPainter painter = switch (type) {
-                        case Hint -> new DefaultHighlighter.DefaultHighlightPainter(Color.LIGHT_GRAY);
-                        case UnknownWord -> new DefaultHighlighter.DefaultHighlightPainter(Color.RED);
-                        case Other -> new DefaultHighlighter.DefaultHighlightPainter(Color.YELLOW);
-                    };
-
-                    try {
-                        h.addHighlight(m.getFromPos(), m.getToPos(), painter);
-                    } catch (final BadLocationException ex) {
-                        throw new AssertionError(ex);
-                    }
-
-                    if (dot >= m.getFromPos() && dot <= m.getToPos()) {
-                        errorList.removeListSelectionListener(errorListListener);
-                        errorList.setSelectedIndex(ruleRow);
-                        errorList.addListSelectionListener(errorListListener);
-                    }
-
-                    ruleRow++;
-                }
-            } catch (final IOException ex) {
-                throw new UncheckedIOException(ex);
-            }
-
-            if (dot != mark) {
-                contentArea.removeCaretListener(contentCaretListener.get());
-                contentArea.setSelectionStart(selectionStart);
-                contentArea.setSelectionEnd(selectionEnd);
-                contentArea.addCaretListener(contentCaretListener.get());
-            }
-        });
-
-        contentArea.addCaretListener(contentCaretListener.get());
-
-        final JFrame mainFrame = new JFrame("Parsimonious Publisher");
-
-        final JComponent metaContainer = new Box(BoxLayout.Y_AXIS);
-
-        final JTable infoTable = new JTable();
-
-        infoTable.setModel(new DefaultTableModel() {
-
-            @Override
-            public int getRowCount() {
-                return 3;
-            }
-
-            @Override
-            public int getColumnCount() {
-                return 2;
-            }
-
-            @Override
-            public boolean isCellEditable(final int rowIndex, final int columnIndex) {
-                return false;
-            }
-
-            @Override
-            public Object getValueAt(final int rowIndex, final int columnIndex) {
-                return switch (rowIndex) {
-                    case 0 -> switch (columnIndex) {
-                        case 0 -> "Characters";
-                        case 1 -> Integer.toString(getText(contentArea).length());
-                        default -> throw new AssertionError();
-                    };
-                    case 1 -> switch (columnIndex) {
-                        case 0 -> "Twitter Characters";
-                        case 1 -> Integer.toString(getTwitterCharacters(getText(contentArea)));
-                        default -> throw new AssertionError();
-                    };
-                    case 2 -> switch (columnIndex) {
-                        case 0 -> "Twitter Characters Remaining";
-                        case 1 -> Integer.toString(280 - getTwitterCharacters(getText(contentArea)));
-                        default -> throw new AssertionError();
-                    };
-                    default -> throw new AssertionError();
-                };
-            }
-
-        });
-
-        metaContainer.add(infoTable);
-
-        final JPanel caretWord = new JPanel();
-        caretWord.add(wordLabel);
-
-        metaContainer.add(caretWord);
-
-        metaContainer.add(new JScrollPane(errorList));
-
-        final JSplitPane mainSplitPane =
-                new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, metaContainer, new JScrollPane(contentArea));
-
-        mainFrame.add(mainSplitPane);
-
-        contentArea.getDocument().addDocumentListener(new DocumentListener() {
-
-            @Override
-            public void insertUpdate(final DocumentEvent e) {
-                ((DefaultTableModel) infoTable.getModel()).fireTableDataChanged();
-            }
-
-            @Override
-            public void removeUpdate(final DocumentEvent e) {
-                ((DefaultTableModel) infoTable.getModel()).fireTableDataChanged();
-            }
-
-            @Override
-            public void changedUpdate(final DocumentEvent e) {
-                ((DefaultTableModel) infoTable.getModel()).fireTableDataChanged();
-            }
-
-        });
-
+        final JFrame mainFrame = new DocumentFrame();
         mainFrame.setSize(800, 600);
         mainFrame.setVisible(true);
     }
